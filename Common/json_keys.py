5abaca44c748fc2a993a7c8c7cb167ea3a2a48bf
EyalIt <31079190+EyalIt@users.noreply.github.com>
2017-08-16 23:05:05 +0300

    Add files via upload

---
+++ Common/json_keys.py
@@ -0,0 +1,7 @@
+
+""" keys that are used by the JSON format between the clients and the server """
+SENSOR_DATA_KEY = 'Sensor Data'
+SENSOR_ORIGIN_KEY = 'Origin'
+SENSOR_VALUE_KEY = 'Value'
+
+CLIENT_ID_KEY = 'Client Id'
